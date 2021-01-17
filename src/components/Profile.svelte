0546d0847927a7f5f65bce06ecf71e3423b37164
Jorge Carrion <jorge.carrion@octopy.com>
2021-01-17 03:20:20 -0600

    profile updated

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,16 +1,13 @@
-<script>
-  export let nickname;
-  export let name;
-</script>
+
 
 <div class="Profile">
   <div class="Profile-content">
     <div class="Profile-avatar">
-      <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+      <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fcf0aff8-3a0c-4f97-97c6-3879137d6c1e/dcn2dfs-938fdacc-cfb0-4300-8556-5d8eefa30969.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZmNmMGFmZjgtM2EwYy00Zjk3LTk3YzYtMzg3OTEzN2Q2YzFlXC9kY24yZGZzLTkzOGZkYWNjLWNmYjAtNDMwMC04NTU2LTVkOGVlZmEzMDk2OS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.emvyWCgCvUXmtBOZIZ9qYzt1j_zzZf1bYWa6PirChdY" alt="">
     </div>
     <div class="Profile-info">
-      <h2>{nickname} </h2>
-      <span>{name} </span>
+      <h2>@jc</h2>
+      <span>Jorge Carrión </span>
     </div>
   </div>
 </div>
--- src/components/Stories.svelte
+++ src/components/Stories.svelte
@@ -7,7 +7,7 @@
     <div class="Stories-items">
       <div class="Stories-item">
         <div class="Stories-item-box">
-          <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+          <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fcf0aff8-3a0c-4f97-97c6-3879137d6c1e/dcn2dfs-938fdacc-cfb0-4300-8556-5d8eefa30969.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZmNmMGFmZjgtM2EwYy00Zjk3LTk3YzYtMzg3OTEzN2Q2YzFlXC9kY24yZGZzLTkzOGZkYWNjLWNmYjAtNDMwMC04NTU2LTVkOGVlZmEzMDk2OS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.emvyWCgCvUXmtBOZIZ9qYzt1j_zzZf1bYWa6PirChdY" alt="">
         </div>
         <h2>
           @jc
