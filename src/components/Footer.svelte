fb205bfee0b3d6127895781ebc51a783c7acaaad
Jorge Carrion <jorge.carrion@octopy.com>
2021-01-17 03:02:09 -0600

    First Commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let year = new Date().getFullYear();
+</script>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+    (c) {year} PUGSTAGRAM FROM GEORGE
+    </div>
+  </div>
+</div>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
